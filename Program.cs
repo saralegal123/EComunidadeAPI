5c90a5332d85509bb2edcb5d221e4f8854401daa
Sara Marcely Andrade de Oliveira <saradomacario@gmail.com>
2025-06-28 01:47:02 -0300

    EComunidadeAPI - Projeto Final //program.cs alterado

--- Program.cs
+++ Program.cs
@@ -1,70 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using EComunidadeAPI.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
- 
+
 var builder = WebApplication.CreateBuilder(args);
- 
+
 builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
-});
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee"));
+    });
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
- 
- builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-   .AddJwtBearer(options =>
-   {
-       options.TokenValidationParameters = new TokenValidationParameters
-       {
-           ValidateIssuerSigningKey = true,
-           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-               .GetBytes(builder.Configuration["ConfiguracaoToken:Chave"])),
-           ValidateIssuer = false,
-           ValidateAudience = false
-       };
-   });
 
-builder.Services.AddAuthorization();
 var app = builder.Build();
- 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
- 
+
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
- 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
- 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
- 
-app.UseAuthorization();        
-app.MapControllers();  
+
+app.MapControllers();
+
 app.Run();
- 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
